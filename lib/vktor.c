c925d1944d0cb03179d5a01e5fb781717e9ded2b
Shahar Evron <shahar.evron@gmail.com>
2009-04-13 23:30:42 +0300

    Basic infrastructure work in progress

--- lib/vktor.c
+++ lib/vktor.c
@@ -1,9 +1,195 @@
+/**
+ * vktor JSON pull-parser library
+ * 
+ * Copyright (c) 2009 Shahar Evron
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-#include <stdio.h>
 #include "vktor.h"
 
-int 
-vktor_test(int number)
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <assert.h>
+
+#define VKTOR_ERR_STRLEN 1024
+
+/**
+ * vktor_parser_init:
+ * 
+ * Initialize and return a new parser struct. Will return NULL if memory can't 
+ * be allocated.
+ */
+vktor_parser*
+vktor_parser_init()
 {
-	return (2 * number);
+	vktor_parser *parser;
+	
+	if ((parser = malloc(sizeof(vktor_parser))) == NULL) {
+		return NULL;
+	}
+		
+	parser->buffer      = NULL;
+	parser->last_buffer = NULL;
+	parser->token_type  = VKTOR_TOKEN_NONE;
+	parser->token_value = NULL;
+	parser->level       = 0;
+	
+	return parser;
+}
+
+/**
+ * vktor_buffer_free: 
+ * @buffer Buffer to free
+ * 
+ * Free a vktor_buffer struct without following any next buffers in the chain. 
+ * Call vktor_buffer_free_all() to free an entire chain of buffers.
+ */
+static void 
+vktor_buffer_free(vktor_buffer *buffer)
+{
+	assert(buffer != NULL);
+	assert(buffer->text != NULL);
+	
+	free(buffer->text);
+	free(buffer);
+}
+
+/**
+ * vktor_buffer_fre_all:
+ * @buffer First buffer in the list to free
+ * 
+ * Free an entire linked list of vktor buffers
+ */
+static void 
+vktor_buffer_free_all(vktor_buffer *buffer)
+{
+	vktor_buffer *next;
+	
+	while (buffer != NULL) {
+		next = buffer->next_buff;
+		vktor_buffer_free(buffer);
+		buffer = next;
+	}
+}
+
+/**
+ * vktor_parser_free:
+ * @parser Parser struct to free 
+ * 
+ * Free parser and any associated memory
+ */
+void
+vktor_parser_free(vktor_parser *parser)
+{
+	assert(parser != NULL);
+	
+	if (parser->buffer != NULL) {
+		vktor_buffer_free_all(parser->buffer);
+	}
+	
+	if (parser->token_value != NULL) {
+		free(parser->token_value);
+	}
+	
+	free(parser);
+}
+
+static void 
+vktor_error_set(vktor_error **eptr, unsigned short code, const char *msg, ...)
+{
+	vktor_error *err;
+	
+	if (eptr == NULL) {
+		return;
+	}
+	
+	if ((err = malloc(sizeof(vktor_error))) == NULL) {
+		return;
+	}
+	
+	err->code = code;
+	err->message = malloc(VKTOR_ERR_STRLEN * sizeof(char));
+	if (err->message != NULL) {
+		va_list ap;
+		      
+		va_start(ap, msg);
+		vsnprintf(err->message, VKTOR_ERR_STRLEN, msg, ap);
+		va_end(ap);
+	}
+	
+	*eptr = err;
+}
+
+void 
+vktor_error_free(vktor_error *err)
+{
+	if (err->message != NULL) {
+		free(err->message);
+	}
+	
+	free(err);
+}
+
+static vktor_buffer*
+vktor_buffer_init(char *text, long text_len)
+{
+	vktor_buffer *buffer;
+	
+	if ((buffer = malloc(sizeof(vktor_buffer))) == NULL) {
+		return NULL;
+	}
+	
+	buffer->text      = text;
+	buffer->size      = text_len;
+	buffer->ptr       = 0;
+	buffer->next_buff = NULL;
+	
+	return buffer;
+}
+
+vktor_status 
+vktor_read_buffer(vktor_parser *parser, char *text, long text_len, 
+                  vktor_error **err) 
+{
+	vktor_buffer *buffer;
+	
+	// Create buffer
+	if ((buffer = vktor_buffer_init(text, text_len)) == NULL) {
+		vktor_error_set(err, 1, "Unable to allocate memory buffer for %ld bytes", 
+			text_len);
+		return VKTOR_ERROR;
+	}
+	
+	// Link buffer to end of parser buffer chain
+	if (parser->last_buffer == NULL) {
+		assert(parser->buffer == NULL);
+		parser->buffer = buffer;
+		parser->last_buffer = buffer;
+	} else {
+		parser->last_buffer->next_buff = buffer;
+		parser->last_buffer = buffer;
+	}
+	
+	return VKTOR_OK;
 }
--- lib/vktor.h
+++ lib/vktor.h
@@ -1,7 +1,82 @@
+/**
+ * vktor JSON pull-parser library
+ * 
+ * Copyright (c) 2009 Shahar Evron
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef _VKTOR_H
 
-int vktor_test(int number);
+/* type definitions */
 
+/* parser status codes */
+typedef enum {
+	VKTOR_ERROR,
+	VKTOR_OK,
+	VKTOR_MORE_DATA
+} vktor_status;
+
+/* parser token types */
+typedef enum {
+	VKTOR_TOKEN_NONE,
+	VKTOR_TOKEN_NULL,
+	VKTOR_TOKEN_BOOL,
+	VKTOR_TOKEN_INT,
+	VKTOR_TOKEN_FLOAT,
+	VKTOR_TOKEN_STRING,
+	VKTOR_TOKEN_ARRAY_START,
+	VKTOR_TOKEN_ARRAY_END,
+	VKTOR_TOKEN_MAP_START,
+	VKTOR_TOKEN_MAP_KEY,
+	VKTOR_TOKEN_MAP_END
+} vktor_token;
+
+typedef struct _vktor_error_struct {
+	unsigned short  code;
+	char           *message;
+} vktor_error;
+
+typedef struct _vktor_buffer_struct {
+	char                        *text;
+	long                         size;
+	long                         ptr;
+	struct _vktor_buffer_struct *next_buff;	
+} vktor_buffer;
+
+typedef struct _vktor_parser_struct {
+	vktor_buffer   *buffer;
+	vktor_buffer   *last_buffer;
+	vktor_token     token_type;
+	void           *token_value;
+	unsigned long   level;
+	// Some configuration options?
+} vktor_parser;
+
+/* function prototypes */
+
+vktor_parser* vktor_parser_init();
+void          vktor_parser_free(vktor_parser *parser);
+void          vktor_error_free(vktor_error *err);
 
 #define _VKTOR_H
 #endif /* VKTOR_H */
