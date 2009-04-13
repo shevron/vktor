2b86d5b8243483e0279fb6a4218d113a28d840bd
Shahar Evron <shahar.evron@gmail.com>
2009-04-13 15:12:52 +0300

    Initial project structure + autotools setup

---
+++ src/vktor.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include "vktor.h"
+
+int 
+vktor_test(int number)
+{
+	return (2 * number);
+}
---
+++ src/vktor.h
@@ -0,0 +1,7 @@
+#ifndef _VKTOR_H
+
+int vktor_test(int number);
+
+
+#define _VKTOR_H
+#endif /* VKTOR_H */
